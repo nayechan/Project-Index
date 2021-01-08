1f71cfe91246aa34b7ee55ea5e1e74691c29ba6a
nayechan <66f@naver.com>
2021-01-08 09:52:45 +0900

    v0.2

--- Assets/Scripts/sample.cs
+++ Assets/Scripts/sample.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sample.cs : MonoBehaviour
+public class Sample : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
