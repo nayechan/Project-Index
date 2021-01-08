5d89de2e617b33a5df16e425198653d237d86b33
nayechan <66f@naver.com>
2021-01-08 09:23:46 +0900

    First Commit

---
+++ Assets/Scripts/Fx_RenderQueue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fx_RenderQueue : MonoBehaviour
+{
+
+    public int m_RenderQueue;
+
+    void Start()
+    {
+        MeshRenderer meshRenderer;
+        Transform[] trans = transform.GetComponentsInChildren<Transform>();
+        foreach (Transform tr in trans)
+        {
+            meshRenderer = tr.gameObject.GetComponent<MeshRenderer>();
+            if (meshRenderer != null && meshRenderer.sharedMaterial != null)
+            {
+                meshRenderer.sharedMaterial.renderQueue = m_RenderQueue;
+            }
+        }
+    }
+}
