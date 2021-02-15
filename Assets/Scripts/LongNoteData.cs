6f1a5061aca9564c35f64c19db07be602537c065
gpfl0712 <harry071212@naver.com>
2021-02-15 10:56:07 +0900

    0215

---
+++ Assets/Scripts/LongNoteData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongNoteData : NoteData
+{
+	float endBeat, endSecond;
+
+	public LongNoteData(float beat, float endBeat, int key) : base(beat, key)
+	{
+		this.endBeat = endBeat;
+	}
+}
